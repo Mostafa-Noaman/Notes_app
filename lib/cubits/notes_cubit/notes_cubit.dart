3ea0dd13f9722005c919f19023cbffb7ad45f33f
Mostafa Noaman <nondy1425@gmail.com>
2023-12-11 23:03:02 +0200

    created notes cubit

---
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import '../../constants.dart';
+import '../../models/notes_model.dart';
+import 'notes_states.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+
+  fetchNotes(NoteModel note) async {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(NotesSuccess(notes));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
+}
