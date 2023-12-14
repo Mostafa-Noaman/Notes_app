17219d37e0db00c719240d5fbbbeed88c428cd0c
Mostafa Noaman <nondy1425@gmail.com>
2023-12-14 02:21:05 +0200

    change the color of notes card with cubit

--- lib/cubits/add_notes_cubit/add_notes_cubit.dart
+++ lib/cubits/add_notes_cubit/add_notes_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
@@ -7,7 +8,10 @@ import 'package:notes_app/models/notes_model.dart';
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
 
+  Color color = const Color(0xffA4036F);
+
   addNote(NoteModel note) async {
+    note.color = color.value;
     emit(AddNotesLoading());
     try {
       var notesBox = Hive.box<NoteModel>(kNotesBox);
